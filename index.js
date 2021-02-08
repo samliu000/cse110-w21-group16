0e2b881230e200b783a7f9eedfa334dfa45910de
samliu000 <44206874+samliu000@users.noreply.github.com>
2021-02-07 20:13:14 -0800

    Added eslint

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 let didShowMessage = false;
+
+function showMessage() {
+    const message = 'I like logs';
+
+    for (let i = 0; i < 5; i++) {
+        console.log(message);
+    }
+}
+
+showMessage();

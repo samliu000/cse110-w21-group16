dcc792205b8d66281bc1a6ed6f4052765ae029a6
memkizz <kwstubbings@protonmail.com>
2021-02-09 15:05:45 -0800

    Updated syntax

--- test/addition_test.js
+++ test/addition_test.js
@@ -4,9 +4,9 @@ var assert = chai.assert;
 describe("Addition", function(){
 	it("adds 1+2 parameters", function(){
 	assert.equal(addition.testAddition(1,2),3);
-	})
+	});
 	it("does not accept string as input", function(){
 		assert.equal(addition.testAddition("hello", "goodbye"), -1);
-	})
+	});
 
 });

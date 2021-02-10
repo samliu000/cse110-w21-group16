96ded9febe481710dd83f1c3cc9d9730a05cb7c9
samliu000 <44206874+samliu000@users.noreply.github.com>
2021-02-09 16:08:06 -0800

    testing

--- addition.js
+++ addition.js
@@ -4,6 +4,7 @@
  * @param {number} num1 - first number to be added
  * @param {number} num2 - second number to be added
  * @return {number} the sum of the two numbers
+ *
  * */
 function testAddition(num1, num2){
 		return num1+num2;

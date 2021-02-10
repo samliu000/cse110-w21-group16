40c00dcac50db5363e108a62c0cca876babb1bfe
samliu000 <44206874+samliu000@users.noreply.github.com>
2021-02-09 16:34:23 -0800

    testing

--- addition.js
+++ addition.js
@@ -4,12 +4,13 @@
  * @param {number} num1 - first number to be added
  * @param {number} num2 - second number to be added
  * @return {number} the sum of the two numbers
- *
  **/
 function testAddition(num1, num2){
+	let hi = 1;
 	return num1+num2;
 }
 
 
 exports.testAddition = testAddition;
 
+sdfssdf

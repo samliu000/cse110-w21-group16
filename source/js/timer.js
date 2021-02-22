fa8986ae40c12e30235ced775de07f8b6c6dce59
Yuru Zhou <yuz101@ucsd.edu>
2021-02-22 10:57:47 -0800

    fixed radio and reset bug

--- source/js/timer.js
+++ source/js/timer.js
@@ -1,12 +1,14 @@
+//timer.js
 
 let countdown = 0; // variable to set/clear intervals
 let session_seconds = 1500; // seconds left on the clock
 let session_minutes = 25;
 let break_minutes = 5;
-let isBreak = true;
+let isBreak = true; 
 let session_count = 1;
 let index;
 let actual;
+let isStarted = false;
 
 const alarm = document.createElement('audio'); // A bell sound will play when the timer reaches 0
 alarm.setAttribute("src", "../alarm/radar_-_ios_7.mp3");
@@ -14,22 +16,23 @@ alarm.setAttribute("src", "../alarm/radar_-_ios_7.mp3");
 
 /* EVENT LISTENERS FOR START AND RESET BUTTONS */
 document.getElementById("start-btn").addEventListener('click', () => {
+  isStarted = true;
   clearInterval(countdown);
   countdown = setInterval(timer, 1);
-  document.getElementById('table').style.display="none";
-  document.getElementById('add-task-container').style.display="none";
   index = getRadioIndex('tSelect');
   actual = document.getElementById('table-content').childNodes[index].childNodes[3].innerHTML;
 });
 
 document.getElementById("reset").addEventListener('click', () => {
-  if(confirm("Are you sure you want to reset?")) {
-  clearInterval(countdown);
-  session_seconds = session_minutes * 60;
-  countdown = 0;
-  isBreak = true;
-  clearInterval(countdown);
-  countdown = setInterval(timer, 10);
+  if(isStarted){
+      if(confirm("Are you sure you want to reset?")) {
+      clearInterval(countdown);
+      session_seconds = session_minutes * 60;
+      countdown = 0;
+      isBreak = true;
+      clearInterval(countdown);
+      countdown = setInterval(timer, 10);
+      }
   }
 });
 
@@ -49,29 +52,30 @@ function timer() {
       }
       session_seconds = break_minutes * 60;
       isBreak = false;
+      isStarted = false;
       actual++;
       session_count ++;
-      
       countdown = setInterval(timer, 10);
+      undoCheck('tSelect');
     }else{
       session_seconds = session_minutes * 60;
       isBreak = true;
-      document.getElementById('table').style.display="block";
-      document.getElementById('add-task-container').style.display="block";
     }
   }
 }
 /* UPDATE HTML CONTENT */
 function countdownDisplay() {
   let session_minutes = Math.floor(session_seconds / 60);
-  let remaindersession_seconds = session_seconds % 60;
-  document.getElementById("timerDisplay").textContent = `${session_minutes}:${remaindersession_seconds < 10 ? '0' : ''}${remaindersession_seconds}`;
+  let remaining_seconds = session_seconds % 60;
+  document.getElementById("timerDisplay").textContent = `${session_minutes}:${remaining_seconds < 10 ? '0' : ''}${remaining_seconds}`;
 }
 
 function updateHTML() {
   countdownDisplay();
-  let x = document.getElementById("table-content").childNodes[index].cells;
-  x[3].innerHTML = actual;
+  if(index >= 0){
+    let x = document.getElementById("table-content").childNodes[index].cells;
+    x[3].innerHTML = actual;
+  }
   if(isBreak == true){
     document.getElementById("status").innerHTML = "Promodoro Session";
   }
@@ -81,10 +85,6 @@ function updateHTML() {
     document.getElementById("status").innerHTML= "Long Break!";
   }
 }
-
-window.setInterval(updateHTML, 100);
-
-document.onclick = updateHTML;
 
 //Radio Index
 function getRadioIndex(name)
@@ -98,3 +98,19 @@ function getRadioIndex(name)
         }
     }
 }
+//
+function undoCheck(name)
+{
+    let elements = document.getElementsByName(name);
+    for (let i = 0, l = elements.length; i < l; i++)
+    {
+        if (elements[i].checked)
+        {
+            elements[i].checked = false;
+        }
+    }
+}
+
+window.setInterval(updateHTML, 100);
+
+document.onclick = updateHTML;

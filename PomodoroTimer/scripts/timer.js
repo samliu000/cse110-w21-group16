db551edeb516a13a42128a3567ce29b8ab7e5c6c
Yuru Zhou <yuz101@ucsd.edu>
2021-02-21 23:08:17 -0800

    reset button bug fixed

--- PomodoroTimer/scripts/timer.js
+++ PomodoroTimer/scripts/timer.js
@@ -1,12 +1,14 @@
+//timer.js
 
 let countdown = 0; // variable to set/clear intervals
 let session_seconds = 1500; // seconds left on the clock
 let session_minutes = 25;
 let break_minutes = 5;
-let isBreak = true;
+let isBreak = true; 
 let session_count = 1;
 let index;
 let actual;
+let isStarted = false;
 
 const alarm = document.createElement('audio'); // A bell sound will play when the timer reaches 0
 alarm.setAttribute("src", "../alarm/radar_-_ios_7.mp3");
@@ -14,6 +16,7 @@ alarm.setAttribute("src", "../alarm/radar_-_ios_7.mp3");
 
 /* EVENT LISTENERS FOR START AND RESET BUTTONS */
 document.getElementById("start-btn").addEventListener('click', () => {
+  isStarted = true;
   clearInterval(countdown);
   countdown = setInterval(timer, 1);
   index = getRadioIndex('tSelect');
@@ -21,13 +24,15 @@ document.getElementById("start-btn").addEventListener('click', () => {
 });
 
 document.getElementById("reset").addEventListener('click', () => {
-  if(confirm("Are you sure you want to reset?")) {
-  clearInterval(countdown);
-  session_seconds = session_minutes * 60;
-  countdown = 0;
-  isBreak = true;
-  clearInterval(countdown);
-  countdown = setInterval(timer, 10);
+  if(isStarted){
+      if(confirm("Are you sure you want to reset?")) {
+      clearInterval(countdown);
+      session_seconds = session_minutes * 60;
+      countdown = 0;
+      isBreak = true;
+      clearInterval(countdown);
+      countdown = setInterval(timer, 10);
+      }
   }
 });
 
@@ -60,8 +65,8 @@ function timer() {
 /* UPDATE HTML CONTENT */
 function countdownDisplay() {
   let session_minutes = Math.floor(session_seconds / 60);
-  let remaindersession_seconds = session_seconds % 60;
-  document.getElementById("timerDisplay").textContent = `${session_minutes}:${remaindersession_seconds < 10 ? '0' : ''}${remaindersession_seconds}`;
+  let remaining_seconds = session_seconds % 60;
+  document.getElementById("timerDisplay").textContent = `${session_minutes}:${remaining_seconds < 10 ? '0' : ''}${remaining_seconds}`;
 }
 
 function updateHTML() {

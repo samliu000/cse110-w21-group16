60d767110eaa96cebe679f29deb4dddb48946e59
Yuru Zhou <yuz101@ucsd.edu>
2021-03-01 04:36:37 -0800

    finish localStorage part

--- source/js/todo.js
+++ source/js/todo.js
@@ -9,6 +9,8 @@ let tName =document.getElementById('tName')
 
 let btn;
 let rad;
+let tasklist = [];
+
 //Added if statement for testing
 if(addT){
 addT.addEventListener("click", checkFields);
@@ -46,6 +48,16 @@ if(est){
 });
 }
 
+window.addEventListener('DOMContentLoaded', () => {
+    // Local storage 
+    let storedTask = localStorage.getItem('tasklist');
+    if(storedTask != null){
+        tasklist = JSON.parse(storedTask);
+        showTaskList();
+    }
+    addT.addEventListener("click", checkFields);
+});
+
 function checkFields(){
 
     if (document.getElementById('tName').value != "" &&
@@ -55,6 +67,49 @@ function checkFields(){
         }
 }
 
+function showTaskList(){
+ 
+    for(let i = 0; i < tasklist.length; i++){
+        //radio button
+       rad = document.createElement('input');
+       rad.type = "radio";
+       rad.id = "radio"+bId;
+       rad.name = "tSelect";
+       rad.onclick = function(){updateCounter(this);};
+       
+   
+       //delete button
+       //try: <i class="fas fa-trash"></i>
+       /*btn = document.createElement('input');
+       btn.value = "Delete";
+       btn.type = "button";
+       btn.id = "edit"+bId;
+       btn.onclick = function() {editRow(this);};*/
+       
+       btn = document.createElement('i');
+       btn.id = tasklist[i].id;
+       btn.className = "fa fa-trash";
+       btn.onclick = function() {editRow(this);};
+       
+       //cells created
+        let row = table.insertRow(-1);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        let cell4 = row.insertCell(3);
+        let cell5 = row.insertCell(4);
+    
+        //cells filled
+        cell1.appendChild(rad);
+        cell2.innerHTML = tasklist[i].taskName;
+        cell3.innerHTML = tasklist[i].estimation;
+        cell4.innerHTML = tasklist[i].actual;
+        cell5.appendChild(btn); // Remove row button
+
+    }
+    document.getElementById("add-form").style.display = "none";
+}
+
 function addTask(){
 
     //radio button
@@ -62,13 +117,13 @@ function addTask(){
     rad.type = "radio";
     rad.id = "radio"+bId;
     rad.name = "tSelect";
-    
+    rad.onclick = function(){updateCounter(this);};
     
 
     //delete button
 	btn = document.createElement('i');
-	btn.id = "edit"+bId;
-	btn.className = "fa fa-trash";
+    btn.id = Date.now();
+    btn.className = "fa fa-trash";
 	btn.onclick = function() {editRow(this);};
 
     //cells created
@@ -86,13 +141,30 @@ function addTask(){
     cell4.innerHTML = 0;
     cell5.appendChild(btn);
 
+    tasklist.push({
+        taskName: document.getElementById("tName").value,
+        estimation: document.getElementById('est').value,
+        actual: 0,
+        id: btn.id
+    });
+
+    localStorage.setItem("tasklist", JSON.stringify(tasklist));
+
     bId++;
+
     document.getElementById("add-form").style.display = "none";
 }
 
 function editRow(elem) {
     let row = elem.parentElement.parentElement;
     row.remove();
+
+    for(let i = 0; i < tasklist.length; i++){
+        if(tasklist[i].id === elem.id){
+             tasklist.splice(i, 1);
+        }
+    }
+    localStorage.setItem("tasklist", JSON.stringify(tasklist));
     
 }
 

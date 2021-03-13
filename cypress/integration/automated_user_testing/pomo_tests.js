88722bfa4ffce6423e20e64fa7a586769ae776d4
memkizz <70921683+memkizz@users.noreply.github.com>
2021-03-13 14:55:36 -0800

    update

--- cypress/integration/automated_user_testing/pomo_tests.js
+++ cypress/integration/automated_user_testing/pomo_tests.js
@@ -52,16 +52,15 @@ describe('Pomodoro Timer Tests', () => {
           });
           });
         it('Check the seconds', () => {
-          cy.visit('/source/index.html ');
-         cy.get('div[id="timerDisplay"]').then((timer) => {
-         const  startTime = timer.text();
-               cy.get('button[id="start-btn"]').click();
+		 cy.visit('/source/index.html ');
+		 cy.get('div[id="timerDisplay"]').then((timer) => {
+		 cy.get('button[id="start-btn"]').click();
        
-          cy.wait(3000).then(() => {
-               const endTime = timer.text();
-               var arrayEnd = endTime.split(":");
-               expect(58).equals(parseInt(arrayEnd[1],10));
-         });
+          	cy.wait(3000).then(() => {
+		       const endTime = timer.text();
+		       var arrayEnd = endTime.split(":");
+		       expect(58).equals(parseInt(arrayEnd[1],10));
+         	});
          });
          });
       

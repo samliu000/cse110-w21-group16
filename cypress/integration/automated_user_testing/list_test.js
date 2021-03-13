6dfc664ecebe79f5886f70a6eaf4d019b9476289
samliu000 <44206874+samliu000@users.noreply.github.com>
2021-03-12 22:39:02 -0800

    Fixed eslint issues

--- cypress/integration/automated_user_testing/list_test.js
+++ cypress/integration/automated_user_testing/list_test.js
@@ -1,28 +1,27 @@
 describe('Automated Button Testing', () => {
-  it('Check the add task button', () => {
-    cy.visit('/source/index.html')
-	cy.contains('+Add Task').click()
-	cy.get('input[id="tName"]').click()
-	.type('example').should('have.value','example')
-	cy.get('button[id="btn-add"]').click()
-	cy.get('table[id=table-content]').contains('example')	
+	it('Check the add task button', () => {
+	cy.visit('/source/index.html');
+	cy.contains('+Add Task').click();
+	cy.get('input[id="tName"]').click().type('example').should('have.value','example');
+	cy.get('button[id="btn-add"]').click();
+	cy.get('table[id=table-content]').contains('example');
+	});
 
-  });
- it('Checks the delete task button', () =>{
-	 cy.visit('/source/index.html')
-	 cy.contains('+Add Task').click()
-	 cy.get('input[id="tName"]').click().type('example')
-	 cy.get('button[id="btn-add"]').click()
-	 cy.get('i[class="fa fa-trash"]').click()
-	 cy.get('example').should('not.exist');
-});
-  it('Check the cancel button', () => {
-	  cy.visit('/source/index.html')
-	 cy.contains('+Add Task').click()
-	 cy.get('input[id="tName"]').click().type('example')
-	  cy.get('button[class="btn-cancel"]').click()
-	  cy.get('example').should('not.exist');
-  });
+	it('Checks the delete task button', () =>{
+		cy.visit('/source/index.html');
+		cy.contains('+Add Task').click();
+		cy.get('input[id="tName"]').click().type('example');
+		cy.get('button[id="btn-add"]').click();
+		cy.get('i[class="fa fa-trash"]').click();
+		cy.get('example').should('not.exist');
+	});
 
+	it('Check the cancel button', () => {
+		cy.visit('/source/index.html');
+		cy.contains('+Add Task').click();
+		cy.get('input[id="tName"]').click().type('example');
+		cy.get('button[class="btn-cancel"]').click();
+		cy.get('example').should('not.exist');
+	});
 });
 

5a54799d9aafca7eebf2df26085641fcc2fc92b1
samliu000 <44206874+samliu000@users.noreply.github.com>
2021-03-13 11:33:39 -0800

    Fixed styling

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,11 +16,11 @@
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  require('@cypress/code-coverage/task')(on, config)
+  require('@cypress/code-coverage/task')(on, config);
 
   // add other tasks to be registered here
 
   // IMPORTANT to return the config object
   // with the any changed environment variables
-  return config
-}
+  return config;
+};
